074432dea4cfeff59a544157593a3731b60ead0a
juanqr08 <jquintero623@gmail.com>
2021-05-30 20:26:56 -0500

    Se agrego Timeline de Experiencia Laboral

--- src/components/cv.jsx
+++ src/components/cv.jsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react';
 import Header from "./Header";
 import Footer from "./Footer";
 import ProfileProfesional from "./ProfileProfetional";
+import { Experience } from "../constants/Expirience";
 import CustomTimeLine from "./TimeLine";
 import Divider from "@material-ui/core/Divider";
 
@@ -11,7 +12,7 @@ const CurriculumVitae = () => {
             <Header title="Hoja de Vida" name="Juan Jose Quintero" position="Desarrollador"/>
             <Divider  />
             <ProfileProfesional />
-            <CustomTimeLine />
+            <CustomTimeLine data={Experience} />
             <Footer title="Juan Jose Quintero Reatiga" description="Ingeniero de Sistemas"/>
         </Fragment>        
     )
---
+++ src/constants/Expirience.jsx
@@ -0,0 +1,26 @@
+export const Experience = [
+    {
+        business: 'Indra Colombia',
+        countMonth: 18, 
+        state: true,
+        position: 'Ingeniero de Sistemas Grado 2 – Líder y Desarrollador para Área de Automatización',
+        functions: 'Liderar el área de automatizaciones, gestionando cada uno de los proyectos que sean comprometidos con las distintas operaciones que se manejen en el Datacenter de Claro, adicional desarrolla soluciones por medio de distintos lenguajes de programación y/o plataformas como Ansible, Jenkis, Docker, ManageIQ, etc.',
+        color: 'primary',
+    },
+    {
+        business: 'Indra Colombia',
+        countMonth: 18, 
+        state: false,
+        position: 'Ingeniero Administrador de Capa Media',
+        functions: 'Administrar servicios de aplicación a nivel de Capa Media, como lo son Weblogic 11g/12c, Tomcat, JBoss, etc. Estos servicios están ubicados en gran mayoría en servidores con Sistemas Operativos Unix, dando paso a la creación de variedad de Shell Scripts, Scripts de Python, que dan mayor agilidad al realizar actividades o al realizar monitoreo de los servicios, adicional automatización de informes generando documentos Word, reducción de tareas y creación de monitores a partir de herramientas como Ansible, reduciendo algunos procesos concurrentes en las operaciones.',
+        color: 'secondary',
+    },
+    {
+        business: 'Practicante del Área de Audiovisuales',
+        countMonth: 11, 
+        state: false,
+        position: 'Fundación Universitaria Unipanamericana',
+        functions: 'Dar soporte técnico en servicios audiovisuales y computacionales, preparando las aulas de estudio y sus correspondientes materiales.',
+        color: 'primary',
+    },
+]
