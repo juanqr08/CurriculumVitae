cc91963f8b626c28e537e6f675b4f6c0fd2471b0
Juan <jquintero623@gmail.com>
2021-07-08 19:00:15 -0500

    Actualizacion de Perfil Profesional

--- src/components/ProfileProfessional.jsx
+++ src/components/ProfileProfessional.jsx
@@ -17,6 +17,7 @@ const ProfileProfessional = (props) => {
             <CardContent className="contentInformationGeneral">
                 <Typography component="h5" variant="h5" className="profileProfessional">
                 Estudiante de Ingeniería de Sistemas cursando onceavo (11) semestre pendiente Practicas, con experiencia en Administración de Servicios de Aplicación (Weblogic, Tomcat, JBoss, etc.), manejo básico sobre Sistemas Operativos Unix/Linux y desarrollador al manejar la célula de automatización en el Datacenter de Claro, proporcionando soluciones a través de lenguajes de programación y Frameworks (Python, Java, Bash, Angular, Django, Ansible, etc.), laborando actualmente como Analista y desarrollador C2, con un buen manejo de herramientas ofimáticas y telemáticas.
+                <br />
                 Con habilidades en toma de decisiones, iniciativa, autonomía mostrando excelentes resultados, eficacia y productividad, con gran interés en diversas áreas de conocimiento para poder actuar de acuerdo a la situación y tener así un criterio adecuado para la resolución de problemas, aplicando los conocimientos actualmente obtenidos para la administración y/o mejora de procesos. Su principal meta es llegar a ser un profesional, donde todos sus aportes muestren un excelente desempeño en su campo.
                 </Typography>
             </CardContent>
